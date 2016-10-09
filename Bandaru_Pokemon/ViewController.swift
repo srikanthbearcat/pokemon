0ed208bdf684c8ad6fcf74f31c40a704b9ed09f5
sreekanth b <sreekanthb@sreekanths-mac.local>
2016-10-09 15:16:21 -0500

    Initial Commit

---
+++ Bandaru_Pokemon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bandaru_Pokemon
+//
+//  Created by sreekanth b on 10/9/16.
+//  Copyright © 2016 sreekanth bandaru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
